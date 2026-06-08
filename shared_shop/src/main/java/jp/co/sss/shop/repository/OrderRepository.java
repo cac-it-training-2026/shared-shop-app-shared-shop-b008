1546db8c8858b45e5a0e9cd76f9ed0c5a6ceb06d
milky245 <xuezhihan@toki.waseda.jp>
2026-06-08 18:04:29 +0900

    Modified ItemRepository and OrderRepository to add 2 Query interface

--- shared_shop/src/main/java/jp/co/sss/shop/repository/OrderRepository.java
+++ shared_shop/src/main/java/jp/co/sss/shop/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.sss.shop.entity.Order;
@@ -24,5 +25,28 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 	 */
 	@Query("SELECT o FROM Order o ORDER BY o.insertDate DESC,o.id DESC")
 	Page<Order> findAllOrderByInsertdateDescIdDesc(Pageable pageable);
+
+	/**
+	 * 会員IDを条件に注文日付降順で注文情報を検索します。
+	 * 秋葉 真穂担当: 注文一覧で利用します。
+	 * @param userId 会員ID
+	 * @param pageable ページング情報
+	 * @return 注文エンティティのページオブジェクト
+	 */
+	@Query("SELECT o FROM Order o WHERE o.user.id =:userId ORDER BY o.insertDate DESC,o.id DESC")
+	Page<Order> findByUserIdOrderByInsertDateDescIdDesc(
+			@Param(value = "userId") Integer userId, Pageable pageable);
+
+	/**
+	 * 注文IDと会員IDを条件に注文情報を検索します。
+	 * 秋葉 真穂担当: 注文詳細で利用します。
+	 * @param id 注文ID
+	 * @param userId 会員ID
+	 * @return 注文エンティティ
+	 */
+	@Query("SELECT o FROM Order o WHERE o.id =:id AND o.user.id =:userId")
+	Order findByIdAndUserId(
+			@Param(value = "id") Integer id,
+			@Param(value = "userId") Integer userId);
 
 }
