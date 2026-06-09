62674d179e26cac472feb4f015ee160be0db70d1
m-akiba <m-akiba@CD26301L.arksystems.co.jp>
2026-06-09 11:47:02 +0900

    add: 注文一覧機能を追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderShowController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderShowController.java
@@ -1,8 +1,10 @@
 package jp.co.sss.shop.controller.client.order;
 
-import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.OrderBean;
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.Order;
+import jp.co.sss.shop.entity.OrderItem;
 import jp.co.sss.shop.repository.OrderRepository;
 import jp.co.sss.shop.service.BeanTools;
 import jp.co.sss.shop.service.PriceCalc;
@@ -57,6 +64,33 @@ public class ClientOrderShowController {
 	@RequestMapping(path = "/client/order/list", method = { RequestMethod.GET, RequestMethod.POST })
 	public String showOrderList(Model model, Pageable pageable) {
 		// TODO 秋葉 真穂担当: ログイン会員IDを条件に注文一覧を取得し、注文Beanリストを画面へ渡す。
+		System.out.println("★★★ showOrderList開始 ★★★");
+		// ログイン会員取得
+		UserBean user = (UserBean) session.getAttribute("user");
+		// 注文情報を注文日時の新しい順に取得
+	    Page<Order> orders = orderRepository.findByUserIdOrderByInsertDateDescIdDesc(user.getId(), pageable);
+	    // Beanクラスにデータ代入
+	    List<OrderBean> orderBeans = new ArrayList<>();
+
+	    for (Order order : orders.getContent()) {
+
+	        OrderBean bean = new OrderBean();
+
+	        bean.setId(order.getId());
+	        // entityクラスのDate型をString型に変更
+	        bean.setInsertDate(order.getInsertDate().toString());
+	        bean.setPayMethod(order.getPayMethod());
+	        // 合計金額の計算
+	        int total = 0;
+	        for (OrderItem orderItem : order.getOrderItemsList()) {
+	            total += orderItem.getPrice() * orderItem.getQuantity();
+	        }
+	        bean.setTotal(total);
+
+	        orderBeans.add(bean);
+	    }
+	    // リクエストスコープに保存
+	    model.addAttribute("orders", orderBeans);
 		return "client/order/list";
 	}
 
